0b8f3122cfcd627d4122355d276b877ec6058f11
AngDM01 <hopro64@gmail.com>
2025-12-04 18:11:12 -0600

    Estructura Api dónde se manejan los controladores, aún no están refactorizados, solo se Dividio la carga en dos controladores

---
+++ BackEnd/CoffeeMachine/Api/Controllers/CoffeeMachineChangeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeMachine.Api.Controllers
+{
+  public class CoffeeMachineChangeController : Controller
+  {
+    [HttpGet("getQuantity")]
+    public ActionResult<Dictionary<string, int>> GetQuantity()
+    {
+      return StatusCode(500, "Aún no implementado");
+    }
+  }
+}
