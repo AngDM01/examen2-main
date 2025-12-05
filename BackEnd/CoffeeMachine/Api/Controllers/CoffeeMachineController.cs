43067fa8d2d30fcc59d0ead2aee0d70e154d4b5e
AngDM01 <hopro64@gmail.com>
2025-12-04 21:23:23 -0600

    Eliminando comentario con código innecesario

--- BackEnd/CoffeeMachine/Api/Controllers/CoffeeMachineController.cs
+++ BackEnd/CoffeeMachine/Api/Controllers/CoffeeMachineController.cs
@@ -22,64 +22,6 @@ namespace CoffeeMachine.Api.Controllers
     [HttpPost("buyCoffee")]
     public ActionResult<string> BuyCoffee([FromBody] OrderCoffeeRequest request)
     {
-      /*
-      if (request.Order == null || request.Order.Count == 0)
-          return BadRequest("Ordem vacia.");
-
-      if (request.Payment.TotalAmount <= 0)
-          return BadRequest("Dinero insuficiente ");
-
-      try
-      {
-          var costoTotal = request.Order.Sum(o => _db.keyValues2.First(c => c.Key == o.Key).Value * o.Value);
-
-          if (request.Payment.TotalAmount < costoTotal)
-          { 
-              return BadRequest("Dinero insuficiente ");
-          }
-
-
-          foreach (var cafe in request.Order)
-          {
-              var selected = _db.keyValues.First(c => c.Key == cafe.Key).Key;
-              if (cafe.Value > _db.keyValues[selected])
-              {
-                  return $"No hay suficientes {selected} en la máquina.";
-              }
-              _db.keyValues[selected] -= cafe.Value;
-          }
-
-          var change = request.Payment.TotalAmount - costoTotal;
-          String result = $"Su vuelto es de: {change} colones. Desglose:";
-
-          foreach (var coin in _db.keyValues3.Keys.OrderByDescending(c => c))
-          {
-              var count = Math.Min(change / coin, _db.keyValues3[coin]);
-              if (count > 0)
-              {
-                  result +=  $" {count} moneda de {coin},  ";              
-                  change -= coin * count;
-              }
-          }
-
-
-          if (change > 0)
-          {
-              return StatusCode(500, "No hay suficiente cambio en la máquina.");
-          }
-
-          return Ok(result);
-      }
-      catch (ArgumentException ex)
-      {
-          return BadRequest(ex.Message);
-      }
-      catch (Exception ex)
-      {
-          return StatusCode(500, ex.Message);
-      }
-      */
-
       return StatusCode(500, "Aún no implementado");
     }
   }
