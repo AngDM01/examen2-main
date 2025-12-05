24c2afacd8e62b64b44cbc6d2c839d60ea283ebd
AngDM01 <hopro64@gmail.com>
2025-12-04 21:27:36 -0600

    Agregando nueva modelo para manejar la compra de los cafés

---
+++ BackEnd/CoffeeMachine/Domain/CoffeeModels/CoffeeOrderItem.cs
@@ -0,0 +1,13 @@
+﻿  namespace CoffeeMachine.Domain.CoffeeModels
+{
+  public class CoffeeOrderItem
+  {
+    public string CoffeeName {  get; set; }
+
+    public int Amount { get; set; }
+
+    public int Price { get; set; }
+
+    public int ActualStock { get; set; }
+  }
+}
